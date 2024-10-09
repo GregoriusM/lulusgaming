da718f8f1d2b4725b63f98df0e41e7519aa45c6d
GregoriusM <gregoriusmario10@gmail.com>
2024-10-09 18:51:49 +0700

    UPDATE GRADLE:MODULE (DEPENDENCY GSON, SWIPE REFRESH)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,15 +42,25 @@ android {
 
 dependencies {
 
-    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    //untuk GSON
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+    //untuk swipe refresh
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
+
+
 
 }
