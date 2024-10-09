64bb77d27948fd6a88bc53062e2d87580ed3c9cf
GregoriusM <gregoriusmario10@gmail.com>
2024-10-10 01:25:59 +0700

    ACC 2 Done (What We Play Page Using Web Service)

--- app/src/main/java/com/ubaya/lulusgaming/view/WhatFragment.kt
+++ app/src/main/java/com/ubaya/lulusgaming/view/WhatFragment.kt
@@ -13,6 +13,8 @@ import com.ubaya.lulusgaming.R
 import com.ubaya.lulusgaming.databinding.FragmentWhatBinding
 import com.ubaya.lulusgaming.viewmodel.GameListViewModel
 
+//ACC 2 DONE
+
 class WhatFragment : Fragment() {
     private lateinit var binding: FragmentWhatBinding
     private lateinit var viewModel: GameListViewModel
